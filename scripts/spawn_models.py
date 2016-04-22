319aeccc10f51cf84367387948030289546f498f
Sam Pfeiffer <sammypfeiffer@gmail.com>
2016-04-22 12:02:29 +0200

    Updated testing scripts to service

--- scripts/spawn_models.py
+++ scripts/spawn_models.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import rospy
-from gazebo_ros_link_attacher.msg import Attach
 from gazebo_msgs.srv import SpawnModel, SpawnModelRequest, SpawnModelResponse
 from copy import deepcopy
 from tf.transformations import quaternion_from_euler
